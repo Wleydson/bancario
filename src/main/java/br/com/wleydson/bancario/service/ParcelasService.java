2669e175580b62b71d7bd8646d51a16060a3b129
wleydson.lemos <wleydsonredes@gmail.com>
2022-06-20 18:56:37 -0300

    removendo classe nao usada

--- src/main/java/br/com/wleydson/bancario/service/ParcelasService.java
+++
@@ -1,9 +0,0 @@
-package br.com.wleydson.bancario.service;
-
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@AllArgsConstructor
-public class ParcelasService {
-}
